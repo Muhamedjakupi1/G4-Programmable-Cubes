dd94b490df5a195ae5a80c6a8edb27e7a89fbe1b
Muhamedjakupi1 <muhamedjakupi056@gmail.com>
2025-11-29 17:55:41 +0100

    continuing programmble cubes udp

--- src/programmable_cubes_UDP.py
+++ src/programmable_cubes_UDP.py
@@ -75,3 +75,97 @@ class programmable_cubes_UDP:
         rb += [-1]
 
         return (lb, rb)
+
+    def get_nix(self):
+        """
+        Get number of integer variables in the chromosome/decision vector.
+
+        Returns:
+            int: number of integer variables.
+        """
+        # the chromosome exists solely of integer variables.
+        return self.setup['max_cmds'] * 2 + 1
+
+    def fitness(self, chromosome, initial_configuration=None, verbose=False):
+        """
+        Fitness function for the UDP
+
+        Args:
+            chromosome: the chromosome/decision vector to be tested
+            verbose: whether to provide more additional output during chromosome evaluation
+        Returns:
+            score: the score/fitness for this chromosome.
+        """
+        # By default, we start from the initial configuration provided in the problem.
+        # This is also the point fitness evaluation on the optimize platform starts from.
+        # During optimization, you can change the initial configuration to start,
+        # e.g., from intermediate configurations.
+        # Note: we do not check here whether a custom cube configuration is valid
+        # (i.e., whether all cubes are connected with each other).
+        if initial_configuration is None:
+            initial_config_path = os.path.join(self.root_dir, self.setup['path'], 'Initial_Config.npy')
+            initial_configuration = np.load(initial_config_path)
+
+        # Create the cube ensemble with an initial cube configuration.
+        cubes = ProgrammableCubes(initial_configuration)
+
+        # Roll-out the command sequence and calculate the final fitness.
+        steps_needed = cubes.apply_chromosome(chromosome, verbose)
+        self.final_cube_positions = cubes.cube_position
+        steps_fraction = steps_needed / self.setup['max_cmds']
+        score = fitness_function(cubes, steps_fraction, self.setup['fitness_offset'],
+                                 self.setup['num_cube_types'], self.initial_cube_types,
+                                 self.target_cube_types, self.target_cube_positions)
+
+        return [-score]
+
+    def pretty(self, chromosome):
+        self.fitness(chromosome, verbose=True)
+
+    def example(self):
+        pass
+
+    def plot(self, which_one, cube_type_to_plot=[0], custom_config=None, custom_cube_types=None):
+        '''
+        Plot the cube ensemble with the default colour scheme given in /problems/*.yaml.
+        Renders the plot without returning anything.
+
+        Args:
+            which_one: What to plot. Options: ['ensemble', 'target'].
+                       'target' plots the target cube positions, i.e., the target shape.
+                       'ensemble' plots the configuration after applying a chromosome (starting from the initial configuration).
+            cube_type_to_plot: list of cube types to plot (int from 0 to maximum number of cube types - 1).
+            custom_config: If not None, this will overwrite the cube positions used for plotting.
+            custom_cube_types: If not None, this will overwrite the cube types used for plotting.
+        '''
+        if which_one == 'ensemble':
+            assert (self.final_cube_positions is not None)
+            positions = self.final_cube_positions
+            cube_types = self.initial_cube_types
+        if which_one == 'target':
+            positions = self.target_cube_positions
+            cube_types = self.target_cube_types
+        if custom_config is not None:
+            positions = custom_config
+        if custom_cube_types is not None:
+            cube_types = custom_cube_types
+
+        cube_tensor = np.zeros((len(cube_type_to_plot),
+                                self.setup['plot_dim'],
+                                self.setup['plot_dim'],
+                                self.setup['plot_dim']))
+        offset = int(np.fabs(np.min(positions)))
+
+        for l in range(len(cube_type_to_plot)):
+            for pos in positions[cube_types == cube_type_to_plot[l]]:
+                i, j, k = pos
+                cube_tensor[l][i + offset][j + offset][k + offset] = 1
+
+        fig = plt.figure()
+        ax = plt.axes(projection='3d')
+        ax.set_facecolor('white')
+        for i in range(len(cube_type_to_plot)):
+            ax.voxels(cube_tensor[i], facecolor=self.setup['colours'][cube_type_to_plot[i]], edgecolors='k', alpha=.4)
+
+        plt.tight_layout()
+        # plt.show()
